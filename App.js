9910b9b1e24b9ed7aef7ce27827e3a43d2458f6b
Saroj <saroj.vrc@gmail.com>
2025-11-02 21:22:31 +0530

    react props

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import resList from "./resList";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img className="logo" src="https://static.vecteezy.com/system/resources/previews/010/628/614/non_2x/food-order-logo-mobile-food-logo-online-food-delivery-logo-vector.jpg" />
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg"
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -16,35 +20,63 @@ const Header = () => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const RestaurantCard = () => {
-  return(
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRatingString,
+    sla,
+    costForTwo,
+  } = resData?.info;
+
+  return (
     <div className="res-card">
-      <h3>Meghna Foods</h3>
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" +
+          cloudinaryImageId
+        }
+        alt="res-logo"
+      />
+      <h4>{name}</h4>
+      <h5>{cuisines.join(", ")}</h5>
+      <h5>{avgRatingString} stars</h5>
+      <h5>{sla.deliveryTime} minutes</h5>
+      <h5>{costForTwo}</h5>
     </div>
   );
-}
+};
 
 const Body = () => {
-  return(
+  return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-conntainer">
-        <RestaurantCard />
+        {resList.map((restaurant) => {
+          return (
+            <RestaurantCard resData={restaurant} key={restaurant.info.id} />
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 const AppLayout = () => {
-  return <div className="app">
-    <Header />
-    <Body />
-  </div>
-}
-
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
