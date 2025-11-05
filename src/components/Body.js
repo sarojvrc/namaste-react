d3fc83bcfe8397671213ac2adb36e46f83a3af3b
Saroj <saroj.vrc@gmail.com>
2025-11-05 16:54:50 +0530

    shimmer-ui added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resData from "../utils/mockData.js";
+import { useState, useEffect } from "react";
+import { SWIGGY_API } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList, setResList] = useState(resData);
-  const filteredList = resList.filter((res) => res.info.avgRating >= 4.5);
+  const [resList, setResList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const swiggyAPI = await fetch(SWIGGY_API);
+    const restaurantList = await swiggyAPI.json();
+    setResList(
+      restaurantList?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+  };
+
+  const filteredList = resList.filter((res) => res.info.avgRating >= 4.5) || [];
+
+  if (resList.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
---
+++ src/components/Shimmer.js
@@ -0,0 +1,92 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+      <div className="shimmer-card">
+        <h4 className="shimmer-name"></h4>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+        <h3 className="shimmer-cuisine"></h3>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
