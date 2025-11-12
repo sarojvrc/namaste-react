c7383b212411ffc10d40a557e1957741e0b2e2bb
Saroj <saroj.vrc@gmail.com>
2025-11-12 21:49:03 +0530

    grocery component

---
+++ src/components/Grocery.js
@@ -0,0 +1,12 @@
+const Grocery = () => {
+  return (
+    <div>
+      <h1>Grocery Page</h1>
+      <h2>Welcome to Grocery Section</h2>
+      <h4>Enjoy Shopping!</h4>
+      <p>Get all your daily needs here.</p>
+    </div>
+  );
+};
+
+export default Grocery;
