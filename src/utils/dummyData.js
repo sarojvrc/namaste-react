ea0bd2cd2553dba9a7684c55d369794b3c555239
Saroj <saroj.vrc@gmail.com>
2025-11-14 11:12:20 +0530

    dummy restaurant menu data

---
+++ src/utils/dummyData.js
@@ -0,0 +1,347 @@
+export const dummyRestaurantData = [
+  {
+    info: {
+      id: "156663",
+      name: "Sri Lakshmi Vaibhava",
+      city: "Bangalore",
+      cloudinaryImageId: "hxdoygnqwwi6i6fenhjo",
+      locality: "Marathahalli",
+      costForTwo: "20000",
+      costForTwoMessage: "₹200 for two",
+      cuisines: ["South Indian"],
+      avgRating: 4.5,
+      parentId: "14682",
+      avgRatingString: "4.5",
+      totalRatingsString: "11K+ ratings",
+      sla: {
+        restaurantId: "156663",
+        deliveryTime: 36,
+        minDeliveryTime: 35,
+        maxDeliveryTime: 40,
+        slaString: "35-40 MINS",
+        lastMileTravelString: "4.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Recommended",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "57015039",
+              name: "Babycorn Pepper Dry",
+              category: "North Indian",
+              description: "Serves 1",
+              imageId: "olgqfifiu6gmqbxln9mz",
+              isVeg: 1,
+              price: 17500,
+
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "6373737",
+              name: "Babycorn Pepper Dry",
+              category: "North Indian",
+              description: "Serves 1",
+              imageId: "olgqfifiu6gmqbxln9mz",
+              isVeg: 1,
+              price: 17500,
+
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "037293",
+              name: "Babycorn Pepper Dry",
+              category: "North Indian",
+              description: "Serves 1",
+              imageId: "olgqfifiu6gmqbxln9mz",
+              isVeg: 1,
+              price: 17500,
+
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "1038281",
+              name: "Babycorn Pepper Dry",
+              category: "North Indian",
+              description: "Serves 1",
+              imageId: "olgqfifiu6gmqbxln9mz",
+              isVeg: 1,
+              price: 17500,
+
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Dosa Special",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "38111893",
+              name: "Masala Dosa ",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId: "qaaukay3nimotywkzufv",
+              inStock: 1,
+              isVeg: 1,
+              price: 8500,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Sweets",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "55623250",
+              name: "Gulab Jamun",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId:
+                "FOOD_CATALOG/IMAGES/CMS/2024/8/31/cd674f8b-6847-4b83-a40d-d0d216c7f18e_2b3ec673-29ea-4d9f-b987-af18747b623b.JPG",
+              inStock: 1,
+              isVeg: 1,
+              price: 3000,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Newly Added",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "83738383",
+              name: "Babycorn Pepper Dry",
+              category: "North Indian",
+              description: "Serves 1",
+              imageId: "olgqfifiu6gmqbxln9mz",
+              isVeg: 1,
+              price: 17500,
+
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Biriyani Special",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "6272772",
+              name: "Masala Dosa ",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId: "qaaukay3nimotywkzufv",
+              inStock: 1,
+              isVeg: 1,
+              price: 8500,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Cakes",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "7383883",
+              name: "Gulab Jamun",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId:
+                "FOOD_CATALOG/IMAGES/CMS/2024/8/31/cd674f8b-6847-4b83-a40d-d0d216c7f18e_2b3ec673-29ea-4d9f-b987-af18747b623b.JPG",
+              inStock: 1,
+              isVeg: 1,
+              price: 3000,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Mutton Special",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "8892",
+              name: "Masala Dosa ",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId: "qaaukay3nimotywkzufv",
+              inStock: 1,
+              isVeg: 1,
+              price: 8500,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    card: {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+      title: "Wines",
+      itemCards: [
+        {
+          card: {
+            "@type": "type.googleapis.com/swiggy.presentation.food.v2.Dish",
+            info: {
+              id: "727282",
+              name: "Gulab Jamun",
+              category: "South Indian Dishes",
+              description: "Serves 1",
+              imageId:
+                "FOOD_CATALOG/IMAGES/CMS/2024/8/31/cd674f8b-6847-4b83-a40d-d0d216c7f18e_2b3ec673-29ea-4d9f-b987-af18747b623b.JPG",
+              inStock: 1,
+              isVeg: 1,
+              price: 3000,
+              itemAttribute: {
+                vegClassifier: "VEG",
+                portionSize: "Serves 1",
+              },
+              showImage: true,
+              itemPriceStrikeOff: true,
+              parentId: "103468316",
+            },
+            hideRestaurantDetails: true,
+          },
+        },
+      ],
+    },
+  },
+];
