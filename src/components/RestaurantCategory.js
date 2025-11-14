0a8cbad149d40dc61b406cf4a065a102d96942f3
Saroj <saroj.vrc@gmail.com>
2025-11-14 11:11:30 +0530

    restaurantCategory component

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+
+const RestaurantCategory = ({ data }) => {
+  const [showItem, setShowItem] = useState(false);
+
+  const handleClick = () => {
+    setShowItem(!showItem);
+  };
+  return (
+    <div>
+      <div className="w-6/12 mx-auto my-2 bg-gray-50 p-4 shadow-lg">
+        <div
+          className="flex justify-between font-bold text-lg cursor-pointer"
+          onClick={handleClick}
+          key={data.title}
+        >
+          <span>
+            {data.title} ({data.itemCards.length}){" "}
+          </span>
+          <span>⬇️</span>
+        </div>
+        {showItem && <ItemList items={data.itemCards} />}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCategory;
