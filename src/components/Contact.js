e5659cb7a4fb8dbce9d8e40be33d26adcb73511c
Saroj <saroj.vrc@gmail.com>
2025-11-19 23:58:14 +0530

    contact with test cases

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,40 @@ const Contact = () => {
   const { loggedInUser } = useContext(UserContext);
 
   return (
-    <div>
+    <div className="text-center">
       <h1 className="text-center font-extrabold text-2xl">Contact Us Page</h1>
-      <h2 className="text-center text-lg">
-        This is Contact us page of Namaste React Project
-      </h2>
+      <h2 className="text-center text-lg">Fill the form to get touch</h2>
+      <div>
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="border bg-amber-50 rounded-lg m-2 p-2"
+            />
+          </div>
+
+          <div>
+            <input
+              type="mobile"
+              placeholder="Enter your number"
+              className="border bg-amber-50 rounded-lg m-2 p-2"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your city"
+              className="border bg-amber-50 rounded-lg m-2 p-2"
+            />
+          </div>
+          <div>
+            <button className="p-2 m-2 border bg-amber-50 rounded-xl cursor-pointer">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
       <h4 className="font-bold text-center">LoggedIn User: {loggedInUser}</h4>
     </div>
   );
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+  const heading = screen.getByRole("heading", { level: 1 });
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should load the button", () => {
+  render(<Contact />);
+  //   const button = screen.getByRole("button");
+  const button = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside Contact Component", () => {
+  render(<Contact />);
+  const nameInput = screen.getByPlaceholderText("Enter your name");
+  const numberInput = screen.getByPlaceholderText("Enter your number");
+  const cityInput = screen.getByPlaceholderText("Enter your city");
+  expect(nameInput).toBeInTheDocument();
+  expect(numberInput).toBeInTheDocument();
+  expect(cityInput).toBeInTheDocument();
+});
